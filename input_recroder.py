864bee5fff7c9bbd7067e6fa00dddff60504b3ab
Gavin <wingman639@gmail.com>
2016-08-01 00:15:28 +0800

    Ctrl+B build & run Python code

--- input_recroder.py
+++ input_recroder.py
@@ -1,7 +1,10 @@
 # -*- coding:UTF-8 -*-
 import wx
+import os
 import time
+import codecs
 from record_data import InputRecord
+import os_cmd
 
 START_TIMESTAMP = time.time()
 CTRL_EFFECT_TIME = 0.2
@@ -35,7 +38,7 @@ class ClientFrame(wx.Frame):
         # self.buttonBox = buttonPanel.ButtonBox(mainPanel)
         # mainSizer.Add(self.buttonBox, proportion=0, flag= wx.TOP, border=5)
 
-        self.mainText = wx.TextCtrl(mainPanel, -1, style=wx.TE_MULTILINE | wx.TE_PROCESS_TAB)
+        self.mainText = wx.TextCtrl(mainPanel, -1, style=wx.TE_MULTILINE | wx.TE_PROCESS_TAB | wx.TE_PROCESS_ENTER)
         mainSizer.Add(self.mainText, proportion= 1, flag=wx.TOP | wx.EXPAND, border=5)
 
         mainPanel.SetSizer(mainSizer)
@@ -69,14 +72,54 @@ class ClientFrame(wx.Frame):
             self.ctrl_time = time.time()
         if event.GetKeyCode() == ord('B') and self.ctrl_time:
             if time.time() - self.ctrl_time < CTRL_EFFECT_TIME:
-                self.infoText.AppendText('Ctrl+B')
+                # self.infoText.AppendText('Ctrl+B: Building...')
+                self.build()
+        event.Skip(True)
 
 
 
 
     #########################
+    def build(self):
+        self.infoTextClean()
+        code_path = self.save()
+        cmd = ['python', code_path]
+        # print cmd
+        start_time = time.time()
+        output = os_cmd.check_output(cmd)
+        end_time = time.time()
+        self.infoText.AppendText(output + '\n')
+        self.infoText.AppendText('[Finished in %.2f]' % (end_time - start_time))
 
 
+    def save(self):
+        # code_path = self._generate_file_path()
+        code_path = 'code.py'
+        text = self.mainText.GetValue()
+        text = self.replace_to_ascii_punctuation(text)
+        with codecs.open(code_path, 'w', 'utf-8') as file:
+            file.write(text)
+        return code_path
+
+    def replace_to_ascii_punctuation(self, text):
+        known_punctuation = {u'‘': '\'',
+                             u'’': '\'',
+                             u'“': '"',
+                             u'”': '"',
+                            }
+        for key, value in known_punctuation.items():
+            text = text.replace(key, value)
+        return text
+
+    def infoTextClean(self):
+        self.infoText.SetValue('')
+        self.infoText.Update()
+
+
+    # def _generate_file_path(self):
+    #     return os.path.join(
+    #                 os.path.dirname(os.path.abspath('__file__')),
+    #                 'code.py')
 
 
 def run_window(record_data):
